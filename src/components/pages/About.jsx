a6e8ac94b4827f7f18089a2ed7c7b0cfd8bfa1b6
Gnanashekar K <gnanashekar045@gmail.com>
2025-04-21 20:49:21 +0530

    second text is add

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -21,6 +21,20 @@ const About = () => {
           magni assumenda? Assumenda veniam ab, officiis obcaecati cumque
           asperiores magnam veritatis ratione exercitationem.
         </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro magnam
+          cum earum ipsam amet aliquid, quis voluptates error. Impedit ratione
+          commodi ipsam? Facilis, ipsum autem? Accusantium necessitatibus
+          corrupti dolorum nisi sit fugit magni consequuntur, laborum nobis
+          dolores laboriosam nesciunt esse, omnis aspernatur illum aliquid,
+          culpa quibusdam ratione facere quidem tempora accusamus voluptate
+          eaque? Omnis numquam at sed corporis quae, molestiae totam earum
+          obcaecati maxime, sunt mollitia exercitationem, molestias repellendus
+          tempora. Voluptas quia impedit reprehenderit vero adipisci cumque
+          eligendi fuga nulla qui minima distinctio soluta, tempora blanditiis
+          repudiandae facere aliquid eius quasi illum laboriosam debitis vel
+          explicabo ullam dolores! Perspiciatis, eaque.
+        </p>
       </Box>
     </Layout>
   );
