1c36d0c6ae611368ec0beb7aef3f2b4010e4c028
Gnanashekar K <gnanashekar045@gmail.com>
2025-04-21 20:48:11 +0530

    text add

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import Layout from '../layout/Layout'
-import { Typography } from '@mui/material'
+import React from "react";
+import Layout from "../layout/Layout";
+import { Typography } from "@mui/material";
 
 const About = () => {
   return (
     <Layout>
-        <Box>
-          <Typography>
-            Wellcome to my restuarent
-          </Typography>
-        </Box>
+      <Box>
+        <Typography>Wellcome to my restuarent</Typography>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi quod
+          quos commodi voluptas, sunt soluta quo. Dolor, consequuntur? Inventore
+          modi qui distinctio amet molestias labore ab enim necessitatibus.
+          Inventore debitis sapiente fugit voluptatibus ratione delectus,
+          excepturi vitae reprehenderit laudantium quae, recusandae quisquam
+          deserunt temporibus quaerat nulla sint. Dignissimos cupiditate beatae
+          iste rem suscipit quam rerum natus itaque, ratione harum laboriosam at
+          voluptatem velit cumque fuga aliquid minima porro culpa voluptates
+          sint atque. Ut excepturi debitis voluptas veniam magnam quaerat
+          laboriosam expedita laborum tenetur dignissimos enim porro impedit,
+          magni assumenda? Assumenda veniam ab, officiis obcaecati cumque
+          asperiores magnam veritatis ratione exercitationem.
+        </p>
+      </Box>
     </Layout>
-  )
-}
+  );
+};
 
-export default About
+export default About;
