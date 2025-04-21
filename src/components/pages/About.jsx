a4fba6428e6887214b7d85bb6fba3fe214230c1c
Gnanashekar K <gnanashekar045@gmail.com>
2025-04-21 20:46:10 +0530

    Typography text added

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Layout from '../layout/Layout'
+import { Typography } from '@mui/material'
 
 const About = () => {
   return (
     <Layout>
         <Box>
-          
+          <Typography>
+            Wellcome to my restuarent
+          </Typography>
         </Box>
     </Layout>
   )
