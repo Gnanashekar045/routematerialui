c5d877a401e212df5d3d54f149fec47cfc566216
Gnanashekar K <gnanashekar045@gmail.com>
2025-04-22 09:29:12 +0530

    Contact component css add

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import Layout from '../layout/Layout'
+import React from "react";
+import Layout from "../layout/Layout";
+import { Box, Typography } from "@mui/material";
 
 const Contact = () => {
   return (
     <Layout>
-        <h1>Contact page</h1>
+      <Box sx={{my:10, ml: 10,}}>
+        <Typography variant="h4">Contact my restaurant</Typography>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
+          excepturi laboriosam dolores dolorum possimus repellat numquam
+          pariatur. Saepe similique animi, ex totam beatae aspernatur aliquid
+          officiis, provident praesentium, officia et?
+        </p>
+      </Box>
     </Layout>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
