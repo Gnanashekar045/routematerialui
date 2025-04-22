c2399c54b25ee399eb5a8f1cc8e0c8e148cabea7
Gnanashekar K <gnanashekar045@gmail.com>
2025-04-22 09:41:04 +0530

    contact css added

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Layout from "../layout/Layout";
-import { Box, Typography } from "@mui/material";
+import { Box, Paper, TableContainer, Typography } from "@mui/material";
 
 const Contact = () => {
   return (
     <Layout>
-      <Box sx={{my:10, ml: 10,}}>
+      <Box sx={{my:10, ml: 10, "& h4": {fontWeight: 'bold', mb:2}}}>
         <Typography variant="h4">Contact my restaurant</Typography>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel
@@ -13,6 +13,11 @@ const Contact = () => {
           pariatur. Saepe similique animi, ex totam beatae aspernatur aliquid
           officiis, provident praesentium, officia et?
         </p>
+      </Box>
+      <Box>
+        <TableContainer component={Paper}>
+
+        </TableContainer>
       </Box>
     </Layout>
   );
